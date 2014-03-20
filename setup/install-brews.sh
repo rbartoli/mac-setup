d5284b05d09974f11b95868dc7bf26276e127877
Riccardo Bartoli <riccardobartoli82@gmail.com>
2014-03-20 17:39:40 +0100

    fixed ntfs support in mac os x 10.9

--- setup/install-brews.sh
+++ setup/install-brews.sh
@@ -30,11 +30,12 @@ brew install tree
 
 # Install NTFS write support
 brew install fuse4x
-sudo /bin/cp -rfX /usr/local/Cellar/fuse4x-kext/0.9.2/Library/Extensions/fuse4x.kext /Library/Extensions
-sudo chmod +s /Library/Extensions/fuse4x.kext/Support/load_fuse4x
+brew unlink fuse4x
 brew install ntfs-3g
 sudo mv /sbin/mount_ntfs /sbin/mount_ntfs.orig
-sudo ln -s /usr/local/Cellar/ntfs-3g/2013.1.13/sbin/mount_ntfs /sbin/mount_ntfs
+sudo ln -s /usr/local/Cellar/ntfs-3g/2014.2.15/sbin/mount_ntfs /sbin/mount_ntfs
+sudo cp -rf /usr/local/Cellar/fuse4x-kext/0.9.2/Library/Extensions/fuse4x.kext /Library/Extensions
+sudo chmod +s /Library/Extensions/fuse4x.kext/Support/load_fuse4x
 
 # Remove outdated versions from the cellar
 brew cleanup
