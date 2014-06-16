81abc742e43af31e7ee36c4178ee088568119079
Riccardo Bartoli <riccardobartoli82@gmail.com>
2014-06-16 21:25:29 +0200

    added nvm

--- setup/install-npms.sh
+++ setup/install-npms.sh
@@ -1,3 +1,12 @@
+# NVM
+git clone https://github.com/creationix/nvm.git ~/.nvm
+nvm install 0.10
+nvm use 0.10
+nvm alias default 0.10
+
+# sudo chown -R $USER /usr/local
+# sudo chown -R `whoami` ~/.npm
+
 # Yeoman and generators
 npm install -g yo
 npm install -g handlebars
