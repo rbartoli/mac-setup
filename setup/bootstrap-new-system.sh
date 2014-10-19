b7a8234e2ca0adca280b96c31c7ae9519715b8d6
Riccardo Bartoli <riccardobartoli82@gmail.com>
2014-10-19 03:41:59 +0200

    Disabled hostname configuration

--- setup/bootstrap-new-system.sh
+++ setup/bootstrap-new-system.sh
@@ -6,14 +6,14 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 6000; kill -0 "$$" || exit; done 2>/dev/null &
 
-echo 'Enter new hostname of the machine (e.g. macbook-pro)'
-    read hostname
-    echo "Setting new hostname to $hostname..."
-    sudo scutil --set HostName "$hostname"
-    compname=$(sudo scutil --get HostName | tr '-' '.')
-    echo "Setting computer name to $compname"
-    sudo scutil --set ComputerName "$compname"
-    sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$compname"
+# echo 'Enter new hostname of the machine (e.g. macbook-pro)'
+#     read hostname
+#     echo "Setting new hostname to $hostname..."
+#     sudo scutil --set HostName "$hostname"
+#     compname=$(sudo scutil --get HostName | tr '-' '.')
+#     echo "Setting computer name to $compname"
+#     sudo scutil --set ComputerName "$compname"
+#     sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$compname"
 
 echo "Installing dotfiles..."
     curl -fsSL https://raw.github.com/rbartoli/dotfiles/master/setup/install.sh | sh
