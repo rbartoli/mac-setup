8aef4dcc742b59314ba3035a79417a716134b6bc
Riccardo Bartoli <riccardobartoli82@gmail.com>
2014-10-22 15:29:30 +0200

    Added missing line to move the font to the fonts directory

--- setup/install-fonts.sh
+++ setup/install-fonts.sh
@@ -5,3 +5,4 @@ brew tap caskroom/fonts
 
 # Install fonts
 wget -O Inconsolata-dz-Powerline.otf https://raw.githubusercontent.com/rbartoli/setup/master/fonts/Inconsolata-dz-Powerline.otf
+mv Inconsolata-dz-Powerline.otf ~/Library/Fonts
