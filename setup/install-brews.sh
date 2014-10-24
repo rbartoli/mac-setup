9fded3d6754e21d13c22904d2a926e6222ec6be2
Riccardo Bartoli <riccardobartoli82@gmail.com>
2014-10-24 13:06:56 +0200

    Added vim, vundle and z

--- setup/install-brews.sh
+++ setup/install-brews.sh
@@ -22,7 +22,9 @@ binaries=(
     grep
     sshuttle
     tree
+    vim
     wget --with-iri
+    z
 )
 
 brew install ${binaries[@]}
@@ -34,6 +36,9 @@ sudo mv /sbin/mount_ntfs /sbin/mount_ntfs.orig
 sudo ln -s /usr/local/sbin/mount_ntfs /sbin/mount_ntfs
 sudo /bin/cp -RfX /usr/local/opt/osxfuse/Library/Filesystems/osxfusefs.fs /Library/Filesystems/
 sudo chmod +s /Library/Filesystems/osxfusefs.fs/Support/load_osxfusefs
+
+# Vim + plugins
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Remove outdated versions from the cellar
 brew cleanup
