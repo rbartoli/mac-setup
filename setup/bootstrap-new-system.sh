45be198e6fffc7e9bf548a0d48af8bd8f1ca01c2
Twig2let <twig2let@gmail.com>
2015-04-18 20:26:49 +0100

    Update bootstrap-new-system.sh

--- setup/bootstrap-new-system.sh
+++ setup/bootstrap-new-system.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 echo "Installing shell..."
-    curl -fsSL https://raw.github.com/rbartoli/setup/master/setup/install-shell.sh | sh
+    curl -fsSL https://raw.github.com/twig2let/setup/master/setup/install-shell.sh | sh
 
 echo "Installing brew packages..."
-    curl -fsSL https://raw.github.com/rbartoli/setup/master/setup/install-brews.sh | sh
+    curl -fsSL https://raw.github.com/twig2let/setup/master/setup/install-brews.sh | sh
 
 echo "Installing ruby gems..."
-    curl -fsSL https://raw.github.com/rbartoli/setup/master/setup/install-gems.sh | sh
+    curl -fsSL https://raw.github.com/twig2let/setup/master/setup/install-gems.sh | sh
     
 echo "Installing node.js packages..."
-    curl -fsSL https://raw.github.com/rbartoli/setup/master/setup/install-npms.sh | sh
+    curl -fsSL https://raw.github.com/twig2let/setup/master/setup/install-npms.sh | sh
 
 echo "Installing fonts..."
-    curl -fsSL https://raw.github.com/rbartoli/setup/master/setup/install-fonts.sh | sh
+    curl -fsSL https://raw.github.com/twig2let/setup/master/setup/install-fonts.sh | sh
     
 echo "Installing native apps..."
-    curl -fsSL https://raw.github.com/rbartoli/setup/master/setup/install-apps.sh | sh
+    curl -fsSL https://raw.github.com/twig2let/setup/master/setup/install-apps.sh | sh
 
 echo "Tweaking OS X..."
-    curl -fsSL https://raw.github.com/rbartoli/setup/master/setup/configure-osx.sh | sh
+    curl -fsSL https://raw.github.com/twig2let/setup/master/setup/configure-osx.sh | sh
 
 echo "Installation completed. Now execute ./osx-for-hackers.sh in the terminal."
